90ef2dedc1cb0a0f442fee85484245ed5b5540de
Lilian <lilianouvmmi@gmail.com>
2021-02-01 15:36:57 +0100

    add .htaccess and error page with function

---
+++ error.php
@@ -0,0 +1,13 @@
+<body>
+  <h1>Agent utilisateur non reconnu ! 😅</h1>
+  <h2>Tu peux toujours admirer notre <b>logo...</b></h2> 
+  <!--
+    https://futureboy.us/stegano/decinput.html
+  -->
+  <script>
+    function decrypt(str) {
+        console.log(atob(str))
+    }
+  </script>
+</body>
+
